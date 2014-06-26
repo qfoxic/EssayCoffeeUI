bdec9ba275ab26622204e3000e8618f88d223f7f
andrew <andrew@a499fdae-f193-11e3-a852-a7a024298e69>
2014-06-26 15:19:12 +0000

    v01

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,18 @@
 $( document ).ready( function( ) {
     // $( '.carousel' ).carousel( );
+    $( '.duration' ).selectpicker( {
+        width: '65%'
+    } );
+    $( '.price_holder' ).on( 'mouseenter', function( ev ) {
+        $( this ).addClass( 'price_holder_hi' );
+        $( this ).find( 'p.title, a.btn' ).each( function( ) {
+            $( this ).addClass( 'bg_hi' );
+        } );
+    } );
+    $( '.price_holder' ).on( 'mouseleave', function( ev ) {
+        $( this ).removeClass( 'price_holder_hi' );
+        $( this ).find( 'p.title, a.btn' ).each( function( ) {
+            $( this ).removeClass( 'bg_hi' );
+        } );
+    } );
 } );
