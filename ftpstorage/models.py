97676fae9a7fb060da4c188bd936f33484c2c449
vpaslav <foxandkamarus@gmail.com>
2014-10-07 12:08:51 -0400

    Fixed unicode filenames

--- ftpstorage/models.py
+++ ftpstorage/models.py
@@ -2,21 +2,35 @@ import os
 import time
 from django.db import models
 from django.contrib.auth.models import User
+import base64
 from general.models import Task, BaseModel
 from django.core.exceptions import ValidationError
+from django.utils.encoding import python_2_unicode_compatible
 
 import constants as co
 
-  
+class MFileField(models.FileField):
+ def pre_save(self, model_instance, add):
+   "Returns field's value just before saving."
+   file = getattr(model_instance, self.attname)
+   if file and not file._committed:
+     # Commit the file to storage prior to saving the model
+     try:
+       filename = file.name
+       base64.b64encode(file.name)
+     except UnicodeEncodeError:
+       filename = 'IB64' + base64.b64encode(file.name.encode('utf-8'))
+     file.save(filename, file, save=False)
+   return file
+
 def get_attach_path(instance, filename):
   #return os.path.join(instance.fowner.username, filename)
   return instance.fowner.username + '/' + filename
 
-
 class Upload(BaseModel):
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
-  attach = models.FileField(upload_to=get_attach_path)
+  attach = MFileField(upload_to=get_attach_path)
   fowner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True,
                             related_name='fowner')
   ftask = models.ForeignKey(Task, on_delete=models.CASCADE, blank=True,
@@ -30,7 +44,10 @@ class Upload(BaseModel):
 
   def get_filename(self):
     try:
-      return self.attach.name and self.attach.name.split('/')[1]
+      filename = self.attach.name and self.attach.name.split('/')[1]
+      if filename.startswith('IB64'):
+        return base64.b64decode(filename.strip('IB64'))
+      return filename
     except:
       return  self.attach.name
 
